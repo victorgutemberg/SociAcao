c498623586df8b94642082163f6368a32afd09b1
Victor <vgom@cin.ufpe.br>
2013-02-27 00:40:07 -0300

    Tratamento do formulario

--- home/models.py
+++ home/models.py
@@ -1,3 +1,5 @@
+# -*- encoding: utf-8 -*-
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -5,20 +7,20 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Usuario(models.Model):
-	nome = models.CharField(max_length=30)
-	email = models.EmailField()
+	user = models.OneToOneField(User)
 	celular = models.ForeignKey('Celular')
 	link_facebook = models.TextField()
-
-	def get_full_name(self):
-		return '%s %s'%(self.nome, self.sobrenome)
+	ciente = models.BooleanField()
 
 	def __unicode__(self):
-		return self.get_full_name()
+		return self.user.get_full_name()
 
 class Celular(models.Model):
 	numero = models.CharField(max_length=15)
 	operadora = models.ForeignKey('Operadora')
+
+	def __unicode__(self):
+		return '%s - %s'%(self.numero, self.operadora.nome)
 
 class Operadora(models.Model):
 	nome = models.CharField(max_length=10)
@@ -34,15 +36,34 @@ class Item(models.Model):
 	camisa = models.ForeignKey(Camisa)
 	quantidade = models.IntegerField()
 
+	def __unicode__(self):
+		return self.camisa.descricao + ' - ' + str(self.quantidade)
+
+class FormaPagamento(models.Model):
+	descricao = models.CharField(max_length=200)
+
+	def __unicode__(self):
+		return self.descricao
+
 class Compra(models.Model):
 	usuario = models.ForeignKey(Usuario)
 	itens = models.ManyToManyField(Item)
 	data = models.DateTimeField(auto_now_add=True)
-	comprovante = models.FileField(upload_to='comprovantes')
+	formaPagamento = models.ForeignKey(FormaPagamento)
+	pago = models.BooleanField(default=False)
+	entregue = models.BooleanField(default=False)
+	#comprovante = models.FileField(upload_to='comprovantes') #file
+
+	def file_name(instance, filename):
+		return '/comprovantes/%s'
 
 	def quantidade_itens(self):
 		return self.itens.count()
 
 	def __unicode__(self):
-		return '%s - %s'%(str(self.data), self.usuario.get_full_name())
+		if self.pago:
+			pago = 'Pago: OK'
+		else:
+			pago = 'Pago: Falta'
+		return '%s - %s - %s'%(self.data.strftime('%d/%m/%Y - %H:%M'), self.usuario.user.first_name, pago)
 
--- home/views.py
+++ home/views.py
@@ -1,7 +1,51 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from models import *
 
 # Create your views here.
-def compra(request):	
+def compra(request):
+	if request.method == 'POST':
+		nome = request.POST.get('nome')
+		email = request.POST.get('email')
+		
+		ciente = request.POST.get('ciente')
+		celular = request.POST.get('celular')
+		operadora = request.POST.get('operadora')
+		facebook = request.POST.get('facebook')
+
+		user = User()
+		user.first_name = nome
+		user.email = email
+		user.username = email[:30]
+		user.save()
+
+		userp = Usuario()
+		userp.user = user
+		userp.ciente = ciente
+		userp.facebook = facebook
+
+		celular = Celular(numero=celular, operadora_id=operadora)
+		celular.save()
+
+		userp.celular = celular
+		userp.save()
+
+		compra = Compra()
+		compra.usuario = userp
+		compra.formaPagamento_id = request.POST.get('conta')
+		compra.save()
+
+		for camisa in [x for x in request.POST if x.startswith('camisa_')]:
+			if request.POST[camisa] != '0':
+				item = Item()
+				item.camisa_id = camisa[7:]
+				item.quantidade = request.POST[camisa]
+				item.save()
+				compra.itens.add(item)
+
+
 	camisas = Camisa.objects.all()
+	operadoras = Operadora.objects.all()
+	contas = FormaPagamento.objects.all()
+
 	return render(request, 'form_compra.html', locals())
